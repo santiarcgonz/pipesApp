11f0dedb24126517087f51e6465bb8095728d99a
santiarcgonz <arcilasantiago@gmail.com>
2023-07-12 23:58:30 -0500

    install primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from "primeng/button";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
